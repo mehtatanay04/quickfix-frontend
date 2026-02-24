e4f2e80de8ac55b852d1bb528109c00e45718f08
Tanay Mehta <mehtatanay40@gmail.com>
2026-02-24 23:11:09 +0530

    Seperated message display according to the role

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,20 @@
-function Dashboard(){
-    return <h1>Welcome To Dashboard</h1>
+function Dashboard() {
+
+    const role = localStorage.getItem("role");
+
+    return (
+        <div>
+            <h1>Dashboard</h1>
+
+            {role === "ADMIN" && (
+                <h2>Welcome Admin Panel</h2>
+            )}
+
+            {role === "USER" && (
+                <h2>Welcome User Dashboard</h2>
+            )}
+        </div>
+    );
 }
 
-export default Dashboard();
+export default Dashboard;
